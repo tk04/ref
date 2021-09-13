7d57a220ed7e8dcb9f91564ba8dca7d678e1510c
tk04 <turkisaloufi@gmail.com>
2021-09-13 19:05:59 -0400

    Use production config from Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,13 +11,20 @@ import Config
 # before starting your production server.
 config :ref, RefWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "reff.netlify.app", port: 443],
+  url: [scheme: "https", host: "fierce-garden-75919.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :ref, Ref.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  url: System.get_env("DATABASE_URL")
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
