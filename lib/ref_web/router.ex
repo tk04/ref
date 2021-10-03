7ae92b23f02e80fec454c155cbb2cfd2ea20513a
tk04 <turkisaloufi@gmail.com>
2021-10-03 18:13:10 -0400

    fixed deployment issues pt2

--- lib/ref_web/router.ex
+++ lib/ref_web/router.ex
@@ -2,8 +2,10 @@ defmodule RefWeb.Router do
   use RefWeb, :router
   use PowAssent.Phoenix.Router
   use Pow.Phoenix.Router
-    use Pow.Extension.Phoenix.Router,
+
+  use Pow.Extension.Phoenix.Router,
     extensions: [PowExtensionOne, PowExtensionTwo]
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -15,13 +17,14 @@ defmodule RefWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+
+    get "/users", UserController, :get_users
   end
 
   pipeline :skip_csrf_protection do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-
   end
 
   # use this pipline for routes that require user_auth to visit
@@ -29,7 +32,6 @@ defmodule RefWeb.Router do
     plug Pow.Plug.RequireAuthenticated,
       error_handler: Pow.Phoenix.PlugErrorHandler
   end
-
 
   scope "/" do
     pipe_through [:browser]
@@ -39,20 +41,18 @@ defmodule RefWeb.Router do
       post "/session", SessionController, :create
       post "/registration", RegistrationController, :create
       get "/auth/paypal/callback", PaypalController, :token
-
     end
 
     pow_session_routes()
     pow_extension_routes()
   end
+
   scope "/" do
     pipe_through :browser
 
     pow_assent_routes()
     pow_routes()
   end
-
-
 
   scope "/", RefWeb do
     pipe_through :browser
@@ -69,10 +69,8 @@ defmodule RefWeb.Router do
 
     live "/comments/new", PostLive.Index, :new_comment
 
-
     resources "/:username/admin", ServiceController
     post "/:username/admin/status", UserController, :commission_status
-
 
     live "/messages/:username", MessageLive.Index, :index
     live "/messages/new", MessageLive.Index, :new
@@ -85,25 +83,17 @@ defmodule RefWeb.Router do
     post "/:username", UserController, :create_follow
     delete "/:username", UserController, :delete_follow
 
-
     live "/:username/requests/:rid", MessageLive.Index, :index
     resources "/:username/:id/requests/", RequestController
 
-
-
-
-
-
-    get     "/:username/:id/requests", RequestController, :index
-    get     "/:username/:id/requests/:id/edit", RequestController, :edit
-    get     "/:username/:id/requests/new", RequestController, :new
-    post    "/:username/:id/requests" , RequestController, :create
-    patch   "/:username/:id/requests/:id" ,RequestController, :update
-    put     "/:username/:id/requests/:id" , RequestController, :update
-    delete  "/:username/:id/requests/:id", RequestController, :delete
-
+    get "/:username/:id/requests", RequestController, :index
+    get "/:username/:id/requests/:id/edit", RequestController, :edit
+    get "/:username/:id/requests/new", RequestController, :new
+    post "/:username/:id/requests", RequestController, :create
+    patch "/:username/:id/requests/:id", RequestController, :update
+    put "/:username/:id/requests/:id", RequestController, :update
+    delete "/:username/:id/requests/:id", RequestController, :delete
   end
-
 
   # Other scopes may use custom stacks.
   # scope "/api", RefWeb do
